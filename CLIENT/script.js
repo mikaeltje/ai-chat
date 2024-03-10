5d5ddae49d1ea5c3750ce23adea9d47ef8ac804e
Mikael <mjpeijer@gmail.com>
2024-03-10 20:32:40 +0100

    klaar

--- CLIENT/script.js
+++ CLIENT/script.js
@@ -10,10 +10,8 @@ class ChatHistory {
         messageElement.textContent = `${sender}: ${message}`;
         chatHistoryElement.appendChild(messageElement);
 
-        // Automatisch scrollen naar het nieuwste bericht
         chatHistoryElement.scrollTop = chatHistoryElement.scrollHeight;
 
-        // Update chatHistory and localStorage
         this.chatHistory.push([sender, message]);
         localStorage.setItem("myChatHistory", JSON.stringify(this.chatHistory));
     }
@@ -26,20 +24,16 @@ class ChatHistory {
 const chatHistory = new ChatHistory();
 
 async function music() {
-    // Invoer van de gebruiker ophalen
     const mood = document.getElementById('music');
     const moodValue = mood.value;
 
-    // De vraag toevoegen aan de chatgeschiedenis
     chatHistory.addToChatHistory(moodValue, 'Jij');
 
-    // De submit-knop en het invoerveld uitschakelen en de loading spinner tonen
     document.getElementById('submit').disabled = true;
-    mood.disabled = true; // Dit schakelt het invoerveld uit
+    mood.disabled = true;
     document.getElementById('loadingSpinner').classList.remove('hidden');
 
     try {
-        // Server aanroepen met fetch() en POST-methode
         const response = await fetch('http://localhost:8000/chat', {
             method: 'POST',
             headers: {
@@ -52,21 +46,15 @@ async function music() {
             throw new Error('Er is een fout opgetreden bij het verwerken van het verzoek.');
         }
 
-        // Het ontvangen antwoord weergeven in de gebruikersinterface
         const data = await response.json();
-        document.getElementById('answer').textContent = data.response; // Aanpassing hier
-        document.getElementById('answer').classList.remove('hidden');
-
-        // Het antwoord toevoegen aan de chatgeschiedenis
-        chatHistory.addToChatHistory(data.response, 'Assistent'); // Aanpassing hier
+        chatHistory.addToChatHistory(data.response, 'Assistent');
+        mood.value = '';
     } catch (error) {
         console.error(error);
-        // Toon een foutmelding als er een fout optreedt
         alert('Er is een fout opgetreden bij het verwerken van het verzoek.');
     } finally {
-        // De submit-knop en het invoerveld inschakelen en de loading spinner verbergen
         document.getElementById('submit').disabled = false;
-        mood.disabled = false; // Dit schakelt het invoerveld weer in
+        mood.disabled = false;
         document.getElementById('loadingSpinner').classList.add('hidden');
     }
 }
